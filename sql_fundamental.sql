856415183687096ccbc5ebeb1ee6d70423e6a744
Nader Safari <nader.safari@proton.me>
2026-07-20 15:52:26 +0300

    trigger really finished

--- sql_fundamental.sql
+++ sql_fundamental.sql
@@ -442,34 +442,84 @@ SELECT SUM(works_with.total_sales) AS total, client_id
  * Triggers 
  */									  
 						  
+DROP TRIGGER my_trigger;
+DROP TRIGGER trigger_one;
+DROP TRIGGER trigger_two;
+DROP TRIGGER trigger_three;
+
+DROP TABLE trigger_test;
+
+DELETE FROM employee WHERE emp_id = 111;
+DELETE FROM employee WHERE emp_id = 110;
+DELETE FROM employee WHERE emp_id = 109;
+
 CREATE TABLE trigger_test(
-	
+
 message VARCHAR(200)
 );					  
-	
+ 
+-- --------------------------
+
 CREATE TRIGGER my_trigger 
 BEFORE INSERT
 ON employee
 FOR EACH ROW
 INSERT INTO trigger_test 
-VALUES('added new employee');
-	
-INSERT INTO employee VALUES(
+VALUES('added new employee - without delimiter');
 
-	110, 
-	'Kevin',
-	'Malone',
-	'1978-02-19',
-	'M',
-	69000,
-	106,
-	3
-);
+-- --------------------------
+
+DELIMITER @@ 
+CREATE TRIGGER trigger_three
+BEFORE INSERT
+ON employee
+FOR EACH ROW 
+BEGIN
+	
+	IF NEW.sex = 'M' THEN
+		INSERT INTO trigger_test VALUES('Added male employee');
+	ELSEIF NEW.sex = 'F' THEN
+		INSERT INTO trigger_test VALUES('Added female employee');
+	ELSE 
+		INSERT INTO trigger_test VALUES('Added wrong gender');
+	END IF;
+END @@
+
+DELIMITER ;
+
+-- --------------------------
+
+DELIMITER @@
+CREATE TRIGGER trigger_two
+BEFORE INSERT
+ON employee
+FOR EACH ROW
+BEGIN
+	INSERT INTO trigger_test VALUES(NEW.first_name);
+END @@
+
+DELIMITER ;
+
+-- --------------------------
+
+DELIMITER $$
+CREATE TRIGGER trigger_one
+BEFORE INSERT
+ON employee
+FOR EACH ROW 
+	BEGIN
+        INSERT INTO trigger_test VALUES('added new employee - with delimiter');
+    END$$
+
+DELIMITER ;
+
+-- --------------------------
+
+INSERT INTO employee VALUES(109, 'Oscar', 'Martinez', '1968-02-19', 'M', 69000, 106, 3);
+INSERT INTO employee VALUES(110, 'Kevin', 'Malone', '1978-02-19', 'L', 69000, 106, 3);
+INSERT INTO employee VALUES(111, 'Nasim', 'Safari', '1995-01-08', 'F', 69000, 106, 3);
 
 SELECT * FROM trigger_test;
-
-
-
 
 
 
